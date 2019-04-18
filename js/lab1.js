8daa5024fc97507fd705c093533f7da982f5be7e
Brockton Sorensen <tin_soldier2008@yahoo.com>
2019-04-18 13:59:14 -0700

    Finished

--- js/lab1.js
+++ js/lab1.js
@@ -5,8 +5,10 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
-    let output = "<h2>Bruce's Skills<h2>";
+    // added ] to end of array
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
+    // added a / to close the second h2
+    let output = "<h2>Bruce's Skills</h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
         output += '<li>' + skills[i] + '</li>';
@@ -16,14 +18,17 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+//    added = and ;
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    // added ;
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
-
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    // added $ before {lastName} {city} and {gpa} so that they would pull from the variable designated.
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+// changed the last " to ' in order to close the string
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
